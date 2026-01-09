ecf4d58ad707c4831979b32cb92c4260feb86a08
RatchetHamster <94993719+RatchetHamster@users.noreply.github.com>
2026-01-09 10:11:37 +0000

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import psutil
 import subprocess
 import docker
 import time
+from pathlib import Path
+from PIL import ImageFont
 from luma.core.interface.serial import i2c
 from luma.oled.device import ssd1306
 from luma.core.render import canvas
