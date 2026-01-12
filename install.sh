e9f686a5140fda53cb2dc93c66f0c0afa1e46b4b
RatchetHamster <94993719+RatchetHamster@users.noreply.github.com>
2026-01-12 12:58:03 +0000

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,13 @@
+#SETUP PYTHON VENV
+python -m venv_front_io /home/pi/python/venvs/
+source /home/pi/python/venvs/venv_front_io/bin/activate
+pip install -r /home/pi/python/NAS_front_display/requirements.txt
+
+#SETUP SERVICE:
+#Move .service file to correct location
+sudo mv front_io.service /etc/systemd/system/
+
+#Enable service at boot
+sudo systemctl daemon-reload
+sudo systemctl enable front_io
+sudo systemctl start front_io
