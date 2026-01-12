2344cef747474ef9193fc1b3e2fd4dab851a4ea3
RatchetHamster <94993719+RatchetHamster@users.noreply.github.com>
2026-01-12 19:31:57 +0000

    Update main.py

--- main.py
+++ main.py
@@ -9,8 +9,6 @@ from luma.oled.device import ssd1306
 from luma.core.render import canvas
 
 #Information interigators:
-def get_ip():
-    return str(subprocess.check_output("ifconfig " + "eth0" + " | awk '/inet / {print $2}'", shell=True)).strip()[2:-3]
     
 def get_cpu_perc_temp():
     cpu_per = str(psutil.cpu_percent()) + '%'
@@ -26,14 +24,10 @@ def get_sd_usage():
     disk = psutil.disk_usage('/')
     disk_tot = round(disk.total/1024.0/1024.0/1024.0,1) # Bytes to GB
     return str(disk.percent) + '%' + ' of ' + str(disk_tot) + 'GB'
+
     
-DOCKER_CLIENT = docker.DockerClient(base_url='unix://var/run/docker.sock')
-def get_container_state(container_name):
-    container = DOCKER_CLIENT.containers.get(container_name)
-    return container.attrs['State']['Status'].capitalize()
-    
-def get_button_status():
-    return subprocess.run(['systemctl', 'is-active', 'pi_button_shutdown.service'], capture_output=True, text=True).stdout.strip().capitalize()
+def get_service_status(service):
+    return subprocess.run(['systemctl', 'is-active', f'{service}.service'], capture_output=True, text=True).stdout.strip().capitalize()
 
 
 #Run Screen and Main File:
@@ -44,14 +38,11 @@ font_s2 = ImageFont.truetype(font_path, 10)
 def get_info(screen=1):
     info = ''
     if screen == 1:
-        info += f'{"IP": <6}{get_ip()}\n'
         info += f'{"CPU": <6}{get_cpu_perc_temp()}\n'
         info += f'{"RAM": <6}{get_mem_usage()}\n'
-        info += f'{"HD": <6}{get_sd_usage()}\n'
+        info += f'{"SD": <6}{get_sd_usage()}\n'
     elif screen == 2:
-        for container in ["portainer", "Plex", "Samba_NAS1", "Samba_NAS2"]:
-            info += f'{container.capitalize(): <11}{get_container_state(container)}\n'
-        info += f'{"Button": <11}{get_button_status()}'
+        info += f'{"Button": <11}{get_service_status("pi_button_shutdown")}'
     return info       
 
 def disp_info(device, screen=1):    
