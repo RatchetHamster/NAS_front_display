a0947df47b752d1dea8227c989282b056bc31056
RatchetHamster <94993719+RatchetHamster@users.noreply.github.com>
2026-01-13 17:38:46 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,59 +1,76 @@
 import psutil
 import subprocess
-import docker
 import time
+import os
 from pathlib import Path
 from PIL import ImageFont
 from luma.core.interface.serial import i2c
 from luma.oled.device import ssd1306
 from luma.core.render import canvas
 
+# SCREEN 1 (system info):
+#    CPU %
+#    CPU Temp
+#    RAM %
+#    SD
+# SCREEN 2 (HDDs):
+#    NAS1: OK/NOT OK usage
+#    NAS2: OK/NOT OK usage
+# SCREEN 3 (Local Services): 
+#    Button
+#    Plex
+# SCREEN 4 (Network services):
+#    AudioPi
+
 #Information interigators:
-    
-def get_cpu_perc_temp():
-    cpu_per = str(psutil.cpu_percent()) + '%'
-    cpu_temp = str(round(psutil.sensors_temperatures()['cpu_thermal'][0].current,1))
-    return cpu_per + ' at ' + cpu_temp + '°C'
-    
+
+def get_cpu_temp():
+    return str(round(psutil.sensors_temperatures()['cpu_thermal'][0].current,1)) + '°C'
+def get_cpu_per():
+    return str(psutil.cpu_percent()) + '%'
 def get_mem_usage():
-    memory = psutil.virtual_memory()
-    mem_tot = round(memory.total/1024.0/1024.0/1024.0,1) # Bytes to GB
-    return str(memory.percent) + '%' + ' of ' + str(mem_tot) + 'GB'
-    
-def get_sd_usage():
-    disk = psutil.disk_usage('/')
+    return str(psutil.virtual_memory().percent)+ '%'
+def get_HDD_usage(path_to_hdd):
+    disk = psutil.disk_usage(path_to_hdd)
     disk_tot = round(disk.total/1024.0/1024.0/1024.0,1) # Bytes to GB
     return str(disk.percent) + '%' + ' of ' + str(disk_tot) + 'GB'
-
-    
 def get_service_status(service):
     return subprocess.run(['systemctl', 'is-active', f'{service}.service'], capture_output=True, text=True).stdout.strip().capitalize()
+def is_mounted(path_to_mount):
+    return os.path.ismount(path_to_mount)
 
 
 #Run Screen and Main File:
 font_path = str(Path(__file__).resolve().parent.joinpath('RobotoMono-Regular.ttf'))
 font_s1 = ImageFont.truetype(font_path, 10)
 font_s2 = ImageFont.truetype(font_path, 10)
+font_s3 = ImageFont.truetype(font_path, 10)
+font_s4 = ImageFont.truetype(font_path, 10)
 
-def get_info(screen=1):
+def screen_info(device, screen=1):
     info = ''
     if screen == 1:
-        info += f'{"CPU": <6}{get_cpu_perc_temp()}\n'
+        font2 = font_s1
+        info += f'{"CPU_%": <6}{get_cpu_perc()}\n'
+        info += f'{"Temp": <6}{get_cpu_temp()}\n'
         info += f'{"RAM": <6}{get_mem_usage()}\n'
         info += f'{"SD": <6}{get_sd_usage()}\n'
-    elif screen == 2:
-        info += f'{"Button": <11}{get_service_status("pi_button_shutdown")}'
-    return info       
 
-def disp_info(device, screen=1):    
-    info = ''
-    if screen==1:
-        info = get_info(screen)
-        font2 = font_s1
-    elif screen==2:
-        info = get_info(screen)
+    elif screen == 2:
         font2 = font_s2
+        for HDD in ["NAS1", "NAS2"]:
+            if is_mounted(f'/mnt/{HDD}'):
+                info += f'{HDD:<5}{get_HDD_usage}\n'
+            else:
+                info += f'{HDD:<5}Not Mounted!'
     
+    elif screen == 3:
+        font2 = font_s3
+        info += f'{"Button": <11}{get_service_status("pi_button_shutdown")}'
+
+    elif screen == 4:
+        font2 = font_s4
+
     with canvas(device, dither=True) as draw:
         draw.text((1, 1), info, font=font2, fill='white')
 
@@ -65,7 +82,7 @@ def main(device):
     while True:
         for screen in (1,2):
             for _ in range(int(screen_time/refresh_time)):
-                disp_info(device, screen)
+                screen_info(device, screen)
                 time.sleep(refresh_time)            
 
 if __name__ == "__main__":
